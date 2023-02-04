3a7ec9fa534c0ae0f9a0f98b0ed4a15a780a7f38
krchandler27 <krchandler27@gmail.com>
2023-02-04 14:43:52 -0700

    Got the notes to save

--- routes/notes.js
+++ routes/notes.js
@@ -1,15 +1,19 @@
 const notes = require('express').Router();
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 const { v4: uuidv4 } = require('uuid');
+let db = require('../db/db.json');
 
 // GET Route for retrieving all the notes
-notes.get('/', (req, res) => {
-  readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)));
+notes.get('/notes', (req, res) => {
+
+console.log(db);
+res.json(db);
+  // readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 // POST Route for a new UX/UI note
-notes.post('/', (req, res) => {
-  console.log(req.body);
+notes.post('/notes', (req, res) => {
+  console.log(db);
 
   const { title, text } = req.body;
 
@@ -19,6 +23,8 @@ notes.post('/', (req, res) => {
       text,
       note_id: uuidv4(),
     };
+    db.push(newnote);
+    console.log(db);
 
     readAndAppend(newnote, '../db/db.json');
     res.json(`note added successfully 🚀`);
