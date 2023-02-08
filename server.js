473783906ce34580a5ac4d1d636a27e2b04ce0ac
krchandler27 <krchandler27@gmail.com>
2023-02-07 20:06:55 -0700

    trying to condense javascript into server.js

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const api = require('./routes/notes.js');
-const html = require('./routes/htmlroutes.js');
-const uuid = require('./helpers/uuid');
-
+const util = require('util');
+const { v4: uuidv4 } = require('uuid');
 const PORT = process.env.port || 3001;
 const app = express();
 
@@ -13,8 +11,62 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.use('/api', api);
-app.use('/', html);
+//Read datebase file and parse it using json
+const findNotes = () => {
+  return readFile('db/db.json', 'utf-8')
+  .then(baseNotes => [].concat(JSON.parse(baseNotes)))
+}
+
+// GET Route to display notes
+notes.get('api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './db/db.json'))
+});
+
+
+// POST Route for adding new note to db
+notes.post('/notes', (req, res) => {
+  const note = JSON.parse(fs.readFileSync('./db/db.json'));
+  console.log(db);
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    note.push(newNote);
+    console.log(db);
+    res.json(`Note added successfully 👍.`)
+  } else {
+    res.error('Error in adding note.');
+  }
+  fs.writeFileSync('./db/db.json', JSON.stringify(note, "utf-8"));
+  res.json(note);
+});
+
+// DELETE Route for old note
+notes.delete('/api/notes/:id', (req, res) => {
+  const note = JSON.parse(fs.readFileSync('./db/db.json'));
+  const removeNote = note.filter((delNote) => delNote.id !== req.params.id);
+  fs.writeFileSync('./db/db.json', JSON.stringify(removeNote));
+  res.json(removeNote);
+});
+
+module.exports = notes;
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} ;) <3`)
